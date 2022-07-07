f245e4131d03f5f4d9bb375155e5b633ec362e1d
vingurzhou <zwz0123460218@icloud.com>
2022-07-07 21:06:51 +0800

    1

---
+++ findMedianSortedArrays/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"sort"
+)
+
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	nums3 := append(nums1, nums2...)
+	sort.Ints(nums3)
+	fmt.Println(nums3)
+	length := len(nums1) + len(nums2)
+	if length%2 == 0 {
+		fmt.Println((nums3[length/2-1] + nums3[length/2]))
+		return float64(float64(nums3[length/2-1]+nums3[length/2]) / 2)
+	} else {
+		return float64(nums3[int(math.Floor(float64(length/2)))])
+	}
+
+}
+
+func main() {
+	fmt.Println(findMedianSortedArrays([]int{1, 2}, []int{3, 4}))
+}
